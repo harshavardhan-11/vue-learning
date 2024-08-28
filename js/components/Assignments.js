688e8c71a123edfff76d2d874ce23d11c191bcdc
Harshavardhan Singam <harshavardahn singam>
2024-08-28 12:16:40 +0530

    Add new assignment input form with functionality to Assignments component

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -7,6 +7,12 @@ export default {
         <section class="space-y-6">
             <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
             <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+            <form @submit.prevent="add">
+                <div class="border border-grey-600">
+                    <input v-model="newAssignment" placeholder="New Assignment..." class="p-2" />
+                    <button type="submit" class="bg-white p-2 border-l">Add</button>
+                </div>
+            </form>
         </section>
     `,
 
@@ -16,7 +22,8 @@ export default {
                 { name: 'Finish project', complete: false, id: 1 },
                 { name: 'Read Chapter 4', complete: false, id: 2 },
                 { name: 'Turn in Homework', complete: false, id: 3 },
-            ]
+            ],
+            newAssignment: ""
         }
     },
 
@@ -26,6 +33,17 @@ export default {
                 inProgress: this.assignments.filter(assignment => ! assignment.complete),
                 completed: this.assignments.filter(assignment => assignment.complete)
             };
+        }
+    },
+    methods: {
+        add() {
+            this.assignments.push({
+                name: this.newAssignment,
+                complete: false,
+                id: this.assignments.length + 1
+            })
+
+            this.newAssignment = "";
         }
     }
 }
